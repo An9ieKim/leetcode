03d27a7908b57d067443613c6bc41f870419ab2e
Eunji Kim <104343498+An9ieKim@users.noreply.github.com>
2023-08-07 18:30:09 +0900

    Time: 42 ms (97.83%), Space: 18 MB (19.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,39 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 자료형 데크로 선언
-        strs: Deque = collections.deque()
+        
+        ### 문제 풀이 1
+#         strs = []
             
-        for char in s:
-            if char.isalnum():
-                strs.append(char.lower())
+#         for char in s:
+#             if char.isalnum():
+#                 strs.append(char.lower())
 
-        while len(strs) > 1:
-            if strs.popleft() != strs.pop():
-                return False
+#         while len(strs) > 1:
+#             if strs.pop(0) != strs.pop():
+#                 return False
 
-        return True
+#         return True
+        
+        
+        
+        ### 문제풀이 2
+        # 자료형 데크로 선언
+#         strs: Deque = collections.deque()
+            
+#         for char in s:
+#             if char.isalnum():
+#                 strs.append(char.lower())
+
+#         while len(strs) > 1:
+#             if strs.popleft() != strs.pop():
+#                 return False
+
+#         return True
+
+
+        ### 문제풀이 3
+        s = s.lower()
+        # 정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]', '', s)
+        
+        return s == s[::-1]
